827186a34216291fc58614fd1a5d30747fe3be66
Senanyo Benwari <benwarisenanyo@yahoo.com>
2024-02-12 17:27:10 +0000

    contextCart

--- src/components/counter/index.tsx
+++ src/components/counter/index.tsx
@@ -1,16 +1,36 @@
 import { IonButton } from '@ionic/react'
 import './index.css'
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { CartContext, CartDataContext } from '../../context/cartContext'
 
-const Counter: React.FC =() => {
-    const [count, SetCount] = useState<number>(0)
+
+interface ItemProp{
+  title: string
+}
+
+const Counter: React.FC<ItemProp> =({title}:ItemProp) => {
+    const { addItem } = useContext(CartDataContext) as CartContext;
+    const [count, SetCount] = useState(0)
+    let qty = count;
+    function decrement(){
+      if(count === 0){
+        SetCount(0);
+      }else {
+        SetCount(count-1);
+      }
+      addItem({title, qty});
+    }
+    function increment(){
+      SetCount(count+1);
+      addItem({title, qty});
+    }
   return (
     <div className='ion-border'>
-        <IonButton fill="clear" size="small" className='ion-btn-wt' onClick={() => count === 0? SetCount(0) : SetCount(count-1)}>
+        <IonButton fill="clear" size="small" className='ion-btn-wt' onClick={() => decrement}>
             - 
         </IonButton>
         <p style={{alignSelf: 'center'}}>{count}</p>
-        <IonButton fill="clear" size="small" className='ion-btn-wt' onClick={() => SetCount(count+1)}>
+        <IonButton fill="clear" size="small" className='ion-btn-wt' onClick={() => increment}>
             +
         </IonButton>
     </div>
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,42 +1,50 @@
 import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import MyCart from '../layout/nav/my_cart';
 import ItemCart from '../components/itemCart';
 import { services, services_w } from '../data'
 import CartModal from '../components/modals/cart_total';
+import { CartContext, CartDataContext } from '../context/cartContext'
 
 
 const CartPage: React.FC = () => {
-   
-
+    const { addToCart } = useContext(CartDataContext) as CartContext;
+    let discount = 500
+    let deliveryFee = 2500
+    let subTotal = addToCart.shirts.price + addToCart.pants.price + addToCart.beddings.price + addToCart.shoes.price 
+    let total = subTotal + deliveryFee - discount
     return (
         <IonPage>
             <MyCart name='My Cart' hide={false}/>
             <IonContent>
-                <ItemCart title={services_w[0].title} 
+                {addToCart.shirts.pcs >= 1 && <ItemCart title={services_w[0].title} 
                     img={services_w[0].image}
-                    pcs={services_w[0].pcs}
-                    price={services_w[0].price}
+                    pcs={addToCart.shirts.pcs}
+                    price={addToCart.shirts.price}
                     modal='remove-modal1'/>
-                <ItemCart title={services_w[1].title} 
+                }
+                {addToCart.pants.pcs >= 1 && <ItemCart title={services_w[1].title} 
                     img={services_w[1].image}
-                    pcs={services_w[1].pcs}
-                    price={services_w[1].price}
+                    pcs={addToCart.pants.pcs}
+                    price={addToCart.pants.price}
                     modal='remove-modal2'/>
-                <ItemCart title={services_w[2].title} 
+                }
+                {addToCart.beddings.pcs >= 1 && <ItemCart title={services_w[2].title} 
                     img={services_w[2].image}
-                    pcs={services_w[2].pcs}
-                    price={services_w[2].price}
+                    pcs={addToCart.beddings.pcs}
+                    price={addToCart.beddings.price}
                     modal='remove-modal3'/>
-                <ItemCart title={services_w[3].title} 
+                }
+                {addToCart.shoes.pcs >= 1 && <ItemCart title={services_w[3].title} 
                     img={services_w[3].image}
-                    pcs={services_w[3].pcs}
-                    price={services_w[3].price}
+                    pcs={addToCart.shoes.pcs}
+                    price={addToCart.shoes.price}
                     modal='remove-modal4'/>
+                }
                 <IonButton id='checkout-modal' style={{width: '54%', '--border-radius': '21px', margin: '5% 23%'}}>
                     Checkout?
                 </IonButton> 
-                <CartModal subTotal={7500} deliveryFee={2500} discount={500} total={9500} />
+                <CartModal subTotal={subTotal} deliveryFee={deliveryFee} discount={discount} total={total} />
             </IonContent>
         </IonPage>
     );
